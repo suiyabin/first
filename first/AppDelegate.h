f4c1921956e39289eccb9dc982ad4b7aa6d17171
suiyabin <1694701029@qq.com>
2015-10-19 11:23:16 +0800

    这是我第一次上传

---
+++ first/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  first
+//
+//  Created by echo42 on 15/10/19.
+//  Copyright © 2015年 echo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
